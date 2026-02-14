da36bea126a4b0e320384ab56ab320df1eec3ca3
RashmikaHarshamal <rashmikaharshamal169@gmail.com>
2026-02-14 14:06:56 +0530

    Update Home.jsx

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -24,36 +24,36 @@ function Home() {
         <SwiperSlide>
           <div className="Header_slidee slide1">
             <div className="content">
-              <small data-swiper-parallax="-200">Career-Map System for computer Engineering</small>
+              <small data-swiper-parallax="-200">Travel Bucket System</small>
               <h2 data-swiper-parallax="-300">
-                Map your <span>Future</span> with smart insights and build the <br />
-                <span>career</span> you’ve always <span>dreamed</span> of
+                Map your <span>Future</span> Travels<br />
+                <span>interesting</span> and <span>well organized</span> of
               </h2>
-              <p data-swiper-parallax="-400">Call Now me <span>9876543210</span></p>
+              <p data-swiper-parallax="-400">Call Now me more details<span>9876543210</span></p>
             </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="Header_slidee slide2">
             <div className="content">
-              <small data-swiper-parallax="-200">Career-Map System for computer Engineering</small>
+              <small data-swiper-parallax="-200">Travel Bucket System</small>
               <h2 data-swiper-parallax="-300">
-                Map your <span>Future</span> with smart insights and build the <br />
-                <span>career</span> you’ve always <span>dreamed</span> of
+                Map your <span>Future</span> Travels<br />
+                <span>interesting</span> and <span>well organized</span> of
               </h2>
-              <p data-swiper-parallax="-400">Call Now <span>9876543210</span></p>
+              <p data-swiper-parallax="-400">Call Now me more details<span>9876543210</span></p>
             </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="Header_slidee slide3">
             <div className="content">
-              <small data-swiper-parallax="-200">Career-Map System for computer Engineering</small>
+              <small data-swiper-parallax="-200">Travel Bucket System</small>
               <h2 data-swiper-parallax="-300">
-                Map your <span>Future</span> with smart insights and build the <br />
-                <span>career</span> you’ve always <span>dreamed</span> of
+                Map your <span>Future</span> Travels<br />
+                <span>interesting</span> and <span>well organized</span> of
               </h2>
-              <p data-swiper-parallax="-400">Call Now <span>9876543210</span></p>
+              <p data-swiper-parallax="-400">Call Now me more details<span>9876543210</span></p>
             </div>
           </div>
         </SwiperSlide>
